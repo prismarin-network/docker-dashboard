96d1295cefc6891e59f08f0e0c061281bd44d41c
Benedikt Kaiser <benediktkaiser8@gmail.com>
2022-01-24 15:00:52 +0100

    Removed Woolgens Button

--- frontend/components/DefaultComponent.tsx
+++ frontend/components/DefaultComponent.tsx
@@ -1,6 +1,5 @@
 import persistentStore from "../stores/PersistentStore";
 import {FC} from "react";
-import {BaseButton} from "@woolgens/web-library";
 
 type SetNameFunction = (newName) => void;
 
@@ -25,9 +24,6 @@ const DefaultComponent: FC<DefaultComponentProps> = ({name, setName}) => {
                     placeholder="Your name..."
                     type="text"
                 />
-                <BaseButton type="primary" className="ml-2" onClick={() => console.info("Button clicked!")}>
-                    Save
-                </BaseButton>
             </div>
         </div>
     )
