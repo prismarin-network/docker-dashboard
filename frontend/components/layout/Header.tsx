0e338e169b3457db91504168c51a53e7bafba680
ReaperMaga <reapermaga@gmail.com>
2022-04-01 21:45:29 +0200

    Added hamburger menu instead of FiIcon, disabled pwa for dev mode (#7)

--- frontend/components/layout/Header.tsx
+++ frontend/components/layout/Header.tsx
@@ -1,27 +1,29 @@
 import {IoLogoDocker} from "react-icons/io5"
 import {FiMenu, FiUser} from "react-icons/fi"
+import Hamburger from "hamburger-react"
 
 declare interface HeaderProps {
     toggleSidebar: () => void;
+    sideBarExtended: boolean
 }
 
-const Header = ({ toggleSidebar }: HeaderProps) => {
+const Header = ({ toggleSidebar, sideBarExtended }: HeaderProps) => {
     return (
-        <div className="flex justify-between fixed top-0 left-0 z-30 bg-blue-400 w-full py-3 px-5">
-            <div className="text-white">
-                <FiMenu className="lg:hidden" onClick={toggleSidebar} size="2rem" />
+        <div className="flex fixed top-0 left-0 z-30 justify-between py-3 px-5 w-full bg-blue-400">
+            <div className="lg:hidden text-white">
+                <Hamburger toggled={sideBarExtended} toggle={toggleSidebar} size={24} />
             </div>
             <a
                 href="https://github.com/prismarin-network/docker-dashboard"
                 target="_blank"
                 className="flex items-center text-white hover:text-gray-100">
                 <IoLogoDocker size="2rem"/>
-                <span className="hidden lg:block font-outfit text-xl ml-2">
+                <span className="hidden lg:block ml-2 text-xl font-outfit">
                     Docker Dashboard
                 </span>
             </a>
-            <div className="text-white">
-                <FiUser className="lg:hidden" size="2rem" />
+            <div className="flex justify-center items-center text-white w-[48px] h-[48px]">
+                <FiUser size="28px" />
             </div>
         </div>
     )
--- frontend/layouts/SidebarLayout.tsx
+++ frontend/layouts/SidebarLayout.tsx
@@ -19,12 +19,12 @@ const SidebarLayout = observer(({ children, ...HTMLElements}: SidebarLayoutProps
 
     return (
         <main>
-            <Header toggleSidebar={toggleSidebar} />
+            <Header toggleSidebar={toggleSidebar} sideBarExtended={sidebarExtended} />
             <div className="flex overflow-hidden h-screen text-gray-700 dark:text-gray-200">
                 <Sidebar extended={sidebarExtended} pathName={router ? router.pathname : ""} />
                 <div onClick={toggleSidebar} className={`${!sidebarExtended && 'hidden'} lg:hidden absolute z-10 w-full h-screen`} />
                 <main className="flex overflow-auto flex-col w-full min-h-screen bg-white dark:bg-bastille-300">
-                    <div className="mt-20 flex-grow mx-7 mb-10">
+                    <div className="flex-grow mx-7 mt-20 mb-10">
                         <div {...HTMLElements} className={`flex-grow ${HTMLElements.className}`}>
                             {children}
                         </div>
